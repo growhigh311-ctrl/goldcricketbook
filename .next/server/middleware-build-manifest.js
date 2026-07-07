03c47878af5541440c794d7302b06ff94c69d49a
growhigh311-ctrl <growhigh311@gmail.com>
2026-07-08 05:07:44 +0530

    Technical SEO Implementation

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/4RU4CWK-LkEqBZQRWp0BS/_buildManifest.js",
+    "static/4RU4CWK-LkEqBZQRWp0BS/_ssgManifest.js",
+    "static/4RU4CWK-LkEqBZQRWp0BS/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/3yiddnbtmgj46.js",
+    "static/chunks/27jktro2p5rq9.js",
+    "static/chunks/2nykiepra7i1k.js",
+    "static/chunks/11oof8oxnxiv9.js",
+    "static/chunks/turbopack-3pa0u6ah_lhy8.js"
+  ]
+};
